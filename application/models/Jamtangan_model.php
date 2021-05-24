da260ec1d5d41098827113335e6826782b836b45
PranandaNurW <widya.nando.wn@gmail.com>
2021-05-24 23:22:27 +0700

    memperbaiki method GET

--- application/models/Jamtangan_model.php
+++ application/models/Jamtangan_model.php
@@ -10,22 +10,4 @@ class Jamtangan_model extends CI_Model
             return $this->db->get_where('product', ['item_id' => $id])->result_array();
         }
     }
-
-    public function deleteJamtangan($id)
-    {
-        $this->db->delete('product', ['item_id' => $id]);
-        return $this->db->affected_rows();
-    }
-
-    public function createJamtangan($data)
-    {
-        $this->db->insert('product', $data);
-        return $this->db->affected_rows();
-    }
-
-    public function updateJamtangan($data, $id)
-    {
-        $this->db->update('product', $data, ['item_id' => $id]);
-        return $this->db->affected_rows();
-    }
 }
