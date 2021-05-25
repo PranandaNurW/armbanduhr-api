496a581904447766b068a8ea8c42eab8a3892a22
PranandaNurW <widya.nando.wn@gmail.com>
2021-05-25 22:53:48 +0700

    menambahkan method POST dan DELETE pada endpoint Jamtangan

--- application/models/Jamtangan_model.php
+++ application/models/Jamtangan_model.php
@@ -10,4 +10,16 @@ class Jamtangan_model extends CI_Model
             return $this->db->get_where('product', ['item_id' => $id])->result_array();
         }
     }
+
+    public function deleteJamtangan($id)
+    {
+        $this->db->delete('product', ['item_id' => $id]);
+        return $this->db->affected_rows();
+    }
+
+    public function createJamtangan($data)
+    {
+        $this->db->insert('product', $data);
+        return $this->db->affected_rows();
+    }
 }
